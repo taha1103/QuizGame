7e79a3790cd26beb06c0f79cf3a941ae918265bd
taha1103 <pakgaming112@gmail.com>
2020-11-29 12:14:56 +0500

    J.S Quiz Game
    Quiz Game

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+    {
+        title: "What should appear at the very end of your JavaScript?",
+        choices: ["The </script>", " The <script>", "The END statement", "None of the above"],
+        answer: "The </script>"
+    },
+    {
+        title: "Which of the following best describes JavaScript?",
+        choices: ["a low-level programming language.", "a scripting language precompiled in the browser.", "a compiled scripting language.", "an object-oriented scripting language."],
+        answer: "an object-oriented scripting language."
+    },
+    ///etc.
+    {
+        title: "Which of the following attribute can hold the JavaScript version?",
+        choices: [" LANGUAGE", "SCRIPT", "VERSION", "None of the above"],
+        answer: " LANGUAGE"
+    },
+    {
+        title: " What is mean by 'this' keyword in javascript?",
+        choices: ["It refers current object", "It referes previous object", "It is variable which contains value", "None of the above"],
+        answer: "It refers current object"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: ["strings", "numbers", "objects", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        title: "Undefined does NOT mean that _______.",
+        choices: ["the variable in the code doesn't exist", "the variable is not assigned to a value", "the variable is assigned no value", "the property doesn't exist"],
+        answer: "the variable is assigned no value"
+    },
+    {
+        title: "Which of the following is not considered a JavaScript operator?",
+        choices: ["new", "this", "delete", "typeof"],
+        answer: "this"
+    }
+];
